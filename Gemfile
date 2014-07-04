a652fc62e6bafc4c3ffed1a8abd1aad3e774bf60
Colin Trent <trentcs@gmail.com>
2014-07-04 15:15:54 -0500

    added bcrypt, authentication, sessions, login, create user

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'activesupport', '~>4.1'
 gem 'activerecord', '~>4.1'
 
 gem 'rake'
+gem 'bcrypt'
 
 group :test do
   gem 'rack-test'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,26 @@
+require "bcrypt"
+
+
 class User < ActiveRecord::Base
   has_many :surveys
   has_many :answers
   has_many :completed_surveys
   has_many :taken_surveys, through: :completed_surveys, source: :survey
   has_many :choices, through: :answers
+
+  include BCrypt
+  # Remember to create a migration!
+  def self.authenticate(email, password)
+
+  end
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
   # Remember to create a migration!
 end
