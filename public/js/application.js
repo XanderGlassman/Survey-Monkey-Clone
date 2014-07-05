0f5603e2d5a349a63cb5a6bec0b668c983550d98
Tyler Keating & Xander Glassman <pair+bontgoy.xanderglassman@devbootcamp.com>
2014-07-05 16:00:56 -0500

    added multiple questions functionality

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  $("#create_choice").hide()
+  $("#create_question").hide()
   $("#create_survey").on("submit" , function(event){
     event.preventDefault();
     $.ajax({
@@ -8,9 +10,10 @@ $(document).ready(function() {
       dataType: "json",
       success: function(result){
         console.log(result);
-        $("#survey_header").prepend("<h3>" + result.survey + "</h3><h4>" + result.question_body + "</h4>")
+        $("#survey_header").prepend("<h3>" + result.survey + "</h3>")
         $("#create_survey").hide();
-        $("#create_choice input[type = 'hidden']").val(result.question_id)
+        $("#create_question input[type = 'hidden']").val(result.survey_id)
+        $("#create_question").show()
       }
     });
   });
@@ -25,6 +28,23 @@ $(document).ready(function() {
         console.log(result);
         $("ul").append("<li>" + result.choice_body + "</li>");
         $("#create_choice input[type = 'text']").val("")
+
+      }
+    });
+  });
+    $("#create_question").on("submit" , function(event){
+    event.preventDefault();
+    $.ajax({
+      url: "/surveys/create_question",
+      type: "POST",
+      data: $(this).serialize(),
+      dataType: "json",
+      success: function(result){
+        console.log(result);
+        $("ul").append("<li><h3>" + result.question_body + "</h3></li>");
+        $("#create_choice input[type = 'hidden']").val(result.question_id)
+        $("#create_question input[type = 'text']").val("")
+        $("#create_choice").show()
       }
     });
   });
